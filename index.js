1241372ffdd43cc4894f221bde722771acf0c92c
Patrick McClernan <patmccler@gmail.com>
2017-10-26 00:30:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function viewCart() {
   if(cart.length > 0) {
     var cart_items_list = "In your cart, you have "
     for(let i = 0; i < cart.length - 1; i++) {
-      cart_items_list += `${cart[i]},`
+      cart_items_list += `${cart[i].cart[i].entries()[0]},`
     }
     cart_items_list += `${cart[cart.length]}`
     console.log(cart_items_list)
