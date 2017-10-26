59bb29f3b8deb33aab694ca0f5425c516e558a03
Patrick McClernan <patmccler@gmail.com>
2017-10-26 22:05:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if(typeof cardNumber) {
+  if(!cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
   }
   else {
