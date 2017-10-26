ab49dcdf7c49332beccfcc1607df440315401c53
Patrick McClernan <patmccler@gmail.com>
2017-10-26 21:40:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
   }
   else {
-      console.log(`Your total cost is $${calcTotal()}, which will be charted to the card ${cardNumber}}.`)
+      console.log(`Your total cost is $${calcTotal()}, which will be charged to the card ${cardNumber}}.`)
       cart = []
   }
 }
