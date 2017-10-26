1e0881f511f5aba6b63a5c83a683a669c291f6d1
Patrick McClernan <patmccler@gmail.com>
2017-10-26 00:41:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,7 +32,9 @@ function viewCart() {
       item = cart[i]
       cart_item_key = Object.keys(item)[0]
 
-      if(i === cart.length - 1 && cart.length > 1)
+      if(i === cart.length - 1 && cart.length > 1) {
+        cart_items_list += 'and'
+      }
 
       cart_items_list += `${cart_item_key} at ${item[cart_item_key]}${i === cart.length - 1 ? "." : ","}`
 
