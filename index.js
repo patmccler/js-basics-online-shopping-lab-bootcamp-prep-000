0688d22603e21d7f7b23c897b52f50969c1755c2
Patrick McClernan <patmccler@gmail.com>
2017-10-26 19:18:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,7 +69,13 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  if(delete card[item])
+  {
+    return cart
+  }
+  else {
+    return "That item is not in your cart."
+  }
 }
 
 function placeOrder(cardNumber) {
